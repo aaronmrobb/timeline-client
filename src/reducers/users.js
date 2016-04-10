151370aef9041ecbea875335205e0b715afbe70e
Aaron Robb <airrobb@gmail.com>
2016-04-10 15:10:13 -0500

    source map and error handling

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -2,5 +2,17 @@ import { toggleLogin } from './navigation'
 
 export function userLogin(state, userdata) {
   const setUser = state.set('user', userdata.uid)
-  return toggleLogin(setUser)
+  const clearError = setUser.setIn(['navigation', 'login', 'error'], false)
+  return toggleLogin(clearError)
+}
+
+export function userLogout(state) {
+  const removeUser = state.set('user', undefined)
+  const removeProjects = removeUser.set('projects', undefined)
+  return removeProjects
+}
+
+export function loginFailure(state) {
+  const setError = state.setIn(['navigation', 'login', 'error'], true)
+  return setError
 }
