3a76b096344548becd500ce7a1f89c9b99a2c220
Aaron Robb <airrobb@gmail.com>
2016-04-11 12:27:03 -0500

    projects rendering

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -11,7 +11,10 @@ export class Project extends Component {
   }
   render() {
     return (
-      <div>this.props.name</div>
+      <div>
+        {this.props.name}
+        {this.props.id}
+      </div>
     )
   }
 }
