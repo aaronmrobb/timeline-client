cfe576250369b5dd1045461df36168d4a5175e35
Aaron Robb <airrobb@gmail.com>
2016-04-23 14:51:59 -0500

    more test scaffold

--- test/components/Home.jsx
+++ test/components/Home.jsx
@@ -4,3 +4,12 @@ import chaiEnzyme from 'chai-enzyme'
 import { Map } from 'immutable'
 
 chai.use(chaiEnzyme())
+
+describe('Nav', () => {
+  xit('renders a pure component', () => {
+
+  })
+  xit('renders correct elements', () => {
+
+  })
+})
