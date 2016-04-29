d2d5a2d3fe548ce66829655cbd01aa429f4c2cfa
Aaron Robb <airrobb@gmail.com>
2016-04-29 12:56:00 -0500

    fucka

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import reactMixin from 'react-mixin'
 import PureRenderMixin from 'react-addons-pure-render-mixin'
 import classnames from 'classnames'
+import ReactAutomaticForm from 'react-automatic-form'
 
 //Sign In shit toggle
 
@@ -28,7 +29,7 @@ export class Login extends Component {
       <div className="nav nav-pills pull-right">
         <button className="btn btn-default navbar-btn" onClick={this.handleClick.bind(this)}>{   this.props.user ? 'Logout' : 'Login'}</button>
         <div className={dropDownClasses}>
-         <form className="form" id="formLogin" onSubmit={this.handleSubmit.bind(this)}>
+          <form className="form" id="formLogin" onSubmit={this.handleSubmit.bind(this)}>
            <div className="form-group">
              <input ref="username" className="form-control" name="username" id="username" type="text" placeholder="Username"/>
            </div>
